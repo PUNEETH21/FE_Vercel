575cd64a328c82d14b708292aff9da91a18d633b
Puneeth Kuntumalla <puneeth.kuntumalla@CaprusIT.local>
2025-12-07 23:48:43 +0530

    2.4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -77,7 +77,7 @@ const Login = () => {
               Welcome Back
             </AuthTitle>
             <Typography variant="body2" color="text.secondary">
-              Sign in to access your healthcare dashboard 67
+              Sign in to access your healthcare dashboard 671
             </Typography>
           </AuthHeaderBox>
           <Box component="form" onSubmit={handleSubmit}>
